ac67e9f3252057460f7bf9c8f007a7ded0465668
Sam-Cordry <samcordry@gmail.com>
2023-07-22 20:55:57 -0400

    Fix bulleted list on temporary main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,13 +12,13 @@ export default function Home() {
   return (
     <div>
         <h1 className='text-center font-bold mt-5 text-xl'>Welcome to the CSH Constitution!</h1>
-        <p className='max-w-20 mt-5 mx-2'>
+        <p className='max-w-20 mt-5 mx-2 px-24'>
           This is the official Constitution for Computer Science House.
           With several accessibility options, pretty much anyone should be able to use it in a way that suits them.
           The different tabs are listed below:
         </p>
         <br />
-        <ul className="pl-32">
+        <ul className="pl-32 list-disc">
             <li><strong>Text:</strong> Allows you to change the font type of the Constitution on the fly to suit your needs.</li>
             <li><strong>Audio:</strong> Allows you to listen to recordings of an AI voice reading the Constitution.</li>
             <li><strong>Search:</strong> Find a section of the Constitution based on search terms instead of looking through the entire thing.</li>
